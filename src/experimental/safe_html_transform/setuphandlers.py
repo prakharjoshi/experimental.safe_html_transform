d2235265a20b25ec8e3ca1a05ced021138743727
Prakhar Joshi <you@example.com>
2015-06-24 01:14:09 +0530

    adding other way to install our new safe_html transform

--- src/experimental/safe_html_transform/setuphandlers.py
+++ src/experimental/safe_html_transform/setuphandlers.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import logging
-from zope.component import getUtility
-from Products.PortalTransforms.interfaces import IPortalTransformsTool
+from StringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 
 
@@ -14,23 +12,38 @@ def post_install(context):
     if isNotCurrentProfile(context):
         return
     # Do something during the installation of this package
-    unregister_transform(context, 'safe_html')
+    site = context.getSite()
+    installPortalTransforms(site)
 
 
-def unregister_transform(context, transform):
-    transform_tool = getUtility(IPortalTransformsTool)
-    if hasattr(transform_tool, transform):
-        transform_tool.unregisterTransform(transform)
+def updateSafeHtml(out, portal):
+    print >> out, 'Update safe_html...'
+    safe_html_id = 'safe_html'
+    safe_html_module = "Products.PortalTransforms.transforms.safe_html"
+    experimental_safe_html_id = 'experimental_safe_html'
+    experimental_safe_html_module = 'experimental.safe_html_transform.safe_html'
+    pt = getToolByName(portal, 'portal_transforms')
+    for id in pt.objectIds():
+        transform = getattr(pt, id)
+        if transform.id == safe_html_id and \
+                transform.module == safe_html_module:
+            try:
+                transform.get_parameter_value('disable_transform')
+            except KeyError:
+                print >> out, '  replace safe_html (%s, %s) ...' % (
+                    transform.name(), transform.module)
+                try:
+                    pt.unregisterTransform(id)
+                    pt.manage_addTransform(experimental_safe_html_id, experimental_safe_html_module)
+                except:
+                    raise
+                else:
+                    print >> out, '  ...done'
+
+    print >> out, '...done'
 
 
-def installTransform(context, logger=None):
-    if logger is None:
-        logger = logging.getLogger('experimental.safe_html_transform')
-    if hasattr(context, 'readDataFile'):
-        if isNotCurrentProfile(context):
-            return
-    else:
-        pass
-    transforms = getToolByName(context, 'portal_transforms')
-    transforms.unregisterTransform('safe_html')
-    transforms.manage_addTransform('safe_html', 'experimental.safe_html_transform.%s' % 'safe_html')
+def installPortalTransforms(portal):
+    out = StringIO()
+
+    updateSafeHtml(out, portal)
